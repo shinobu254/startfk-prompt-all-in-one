40968ab2d083b18ef40a00e8b03c3f7db19122f2
RabuHoteru <41131937+shinobu254@users.noreply.github.com>
2023-10-12 17:34:29 -0400

    Add files via upload

---
+++ src/globals.js
@@ -0,0 +1,14 @@
+export default {
+    name: 'shinobu254/startfk-prompt-all-in-one',
+    shortName: 'startfk-prompt-all-in-one',
+    github: 'https://github.com/shinobu254/startfk-prompt-all-in-one',
+    nameAssets: 'shinobu254/startfk-prompt-all-in-one-assets',
+    shortNameAssets: 'startfk-prompt-all-in-one-assets',
+    githubAssets: 'https://github.com/shinobu254/startfk-prompt-all-in-one-assets',
+    gitee: 'https://gitee.com/shinobu254/startfk-prompt-all-in-one',
+    docs: 'https://aiodoc.physton.com',
+    nameTagComplete: 'shinobu254/startfk-tagcomplete',
+    shortNameTagComplete: 'startfk-tagcomplete',
+    githubTagComplete: 'https://github.com/shinobu254/startfk-tagcomplete',
+    civitaiUrl: 'https://civitai.com',
+}
